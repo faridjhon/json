7e3f1c98e5ebf5a68a09d1b6dc0e494c0e5bcf32
farid <faridjhon@gmail.com>
2021-02-08 23:25:21 +0600

    final

--- index.js
+++ index.js
@@ -1,41 +1,37 @@
-const searchButton = document.getElementById('search-btn');
+const searchBtn = document.getElementById('search-btn');
 const mealList = document.getElementById('meal');
-const mealDetailstext = document.querySelector('.meal-details-content');
-const recipeCloseBtn = document.getElementById('recipe-close-btn');
-
-searchButton.addEventListener('click ', getmealList);
+// event listeners
+searchBtn.addEventListener('click', getMealList);
 
 
-function getmealList(){
 
-    let searchInput = document.getElementById('seachinput').nodeValue.trim();
-    fetch ('https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast')
-    .then (res => res.json())
-    .then (data =>{
+// get meal 
+function getMealList(){
+    let searchInputTxt = document.getElementById('search-input').value;
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+    .then(res => res.json())
+    .then(data => {
         let html = "";
         if(data.meals){
-            data.meals.forEach(meal =>{
-                if(data.meals){
-                    data.meals.forEach(meal => {
-                        html += `
-                            <div class = "meal-item" data-id = "${meal.idMeal}">
-                                <div class = "meal-img">
-                                    <img src = "${meal.strMealThumb}" alt = "food">
-                                </div>
-                                <div class = "meal-name">
-                                    <h3>${meal.strMeal}</h3>
-                                    <a href = "#" class = "recipe-btn">Get Recipe</a>
-                                </div>
-                            </div>
-                        `;
-            })
+            data.meals.forEach(meal => {
+                html += `                    
+                        <div class = "meal-img" >
+                            <img src = "${meal.strMealThumb}" >
+                        </div>
+                        <div class = "meal-name">
+                            <h3>${meal.strMeal}</h3>                            
+                        </div>                    
+                `;
+            });
+            mealList.classList.remove('notFound');
+        } else{
+            html = "Sorry, we didn't find any Item!";
+            mealList.classList.add('Sorry!!!!!');
         }
 
-        
-
-
-
-
-
-    })
+        mealList.innerHTML = html;
+    });
 }
+
+
+
